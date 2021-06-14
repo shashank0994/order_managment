d4f081c6d8cd345a8919f80bed07b171138bdc43
Shashank <shashank@artworksit.com>
2021-06-14 20:13:33 +0530

    Order management application

--- routes/web.php
+++ routes/web.php
@@ -16,10 +16,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return redirect('products');
 });
+// Products Routes
 Route::get('products',[\App\Http\Controllers\ProductController::class,'product_list']);
+
+// Carts Routes
 Route::get('cart',[\App\Http\Controllers\CartController::class,'cart_list']);
 Route::post('cart/{product_id}',[\App\Http\Controllers\CartController::class,'save_cart']);
 Route::post('edit-cart/{cart_id}/{action}',[\App\Http\Controllers\CartController::class,'edit_cart']);
+
+// Orders Routes
 Route::get('orders',[\App\Http\Controllers\OrderController::class,'order_list']);
 Route::post('order/{action}',[\App\Http\Controllers\OrderController::class,'save_order']);
 Route::post('retry/payment/{order_number}',[\App\Http\Controllers\OrderController::class,'retry_payment']);
